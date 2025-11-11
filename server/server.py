ffc5d58a61ed4ddd81f9881361937601f939f709
du an <tranvuanhtuan160205@tuan160205gmail.60205gmailcom>
2025-11-11 10:21:43 +0700

    Cập nhật code server.py

--- server/server.py
+++ server/server.py
@@ -20,13 +20,3 @@ class CalculatorServicer(calculator_pb2_grpc.CalculatorServiceServicer):
             return calculator_pb2.CalcReply()
         return calculator_pb2.CalcReply(result=request.num1 / request.num2)
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    calculator_pb2_grpc.add_CalculatorServiceServicer_to_server(CalculatorServicer(), server)
-    server.add_insecure_port('[::]:50051')
-    print("✅ gRPC Server đang chạy tại cổng 50051 ...")
-    server.start()
-    server.wait_for_termination()
-
-if __name__ == '__main__':
-    serve()
